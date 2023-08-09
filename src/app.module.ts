029140078d6f59e42b77485964561ea97c2268f5
lxy-Jason <1627962483@qq.com>
2023-08-09 23:30:03 +0800

    feat: 数据库连接成功&添加swagger文档

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// 引入 Mongoose 
+// 引入 Mongoose
 import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/blog')],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1', {
+      dbName: 'blog',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('lxy-blog-api')
+    .setDescription('博客接口文档')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api-docs', app, document);
   await app.listen(3667);
 }
+
 bootstrap();
