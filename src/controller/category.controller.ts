209435ef9a03809edd8cace8f757a28c517840a7
lxy-Jason <1627962483@qq.com>
2023-11-18 22:21:06 +0800

    feat: 文档UI界面完善

--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -1,4 +1,4 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CategoryProvider } from '../provider/category/category.provider';
 import { Controller, Get } from '@nestjs/common';
 import { getArticleCategory } from '../utils/addArticleByNote';
@@ -8,6 +8,10 @@ import { getArticleCategory } from '../utils/addArticleByNote';
 export class CategoryController {
   constructor(private readonly categoryProvider: CategoryProvider) {}
 
+  @ApiOperation({
+    summary: '更新所有分类',
+    description: '更新所有分类',
+  })
   @Get('updateAllCategory')
   async updateAllCategory() {
     const categorys = getArticleCategory();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { knife4jSetup } from 'nest-knife4j';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,9 +12,17 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/api-docs', app, document);
+  SwaggerModule.setup('api', app, document);
+  knife4jSetup(app, [
+    {
+      name: '1.X版本',
+      url: `/api-json`,
+      swaggerVersion: '1.0',
+      location: `/api-json`,
+    },
+  ]);
   await app.listen(3667);
-  console.log('文档地址:' + `http://localhost:3667/api-docs`);
+  console.log('文档地址:' + `http://localhost:3667/doc.html#/home`);
 }
 
 bootstrap();
